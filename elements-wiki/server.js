19f9deb725acfcea08f73156a9056d6365c57cae
LachsFreund <Jannik.hildenbrand09@gmail.com>
2026-06-25 21:41:16 +0200

    Add files via upload

---
+++ elements-wiki/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("."));
+
+app.get("/load", (req, res) => {
+
+    const data = fs.readFileSync("saves.json", "utf8");
+
+    res.send(data);
+});
+
+app.post("/save", (req, res) => {
+
+    fs.writeFileSync(
+        "saves.json",
+        JSON.stringify(req.body, null, 4)
+    );
+
+    res.send({ success: true });
+});
+
+app.listen(3000, () => {
+    console.log("Server läuft auf Port 3000");
+});
